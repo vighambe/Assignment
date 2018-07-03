2adde0d01d5e6be62118eb8a16d188fb71a8dba7
Vighnesh Ambekar <vighnesh.ambekar@gmail.com>
2018-07-03 10:34:08 +0530

    updated

---
+++ ContactManagement/ContactManagement.Console/Program.cs
@@ -0,0 +1,17 @@
+﻿using ContactManagement.Lib;
+using System;
+
+namespace ContactManagement.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Logger log = Logger.GetLoggerObject(@"c:\test.txt", @"c:\error.txt");
+            log.WriteLog("Test Log");
+            log.WriteError("Test error log");
+            Console.Read();
+        }
+    }
+}
